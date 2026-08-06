65456f2939b60abad5123fe5d27b9283277012b9
Jensen <2224779401@qq.com>
2026-08-06 12:04:45 +0800

    Add files via upload

--- conset.cpp
+++ conset.cpp
@@ -2,15 +2,14 @@
 
 int main()
 {
-	const int AMOUNT = 100;
-	int price = 0;
-	
-	printf("请输入金额（元）；");
-	scanf("%d", &price);
-	
-	int change = AMOUNT - price;
-	
-	printf("找您%d元。\n", change);
+	int a=5;
+	int b=6;
+	int t;
+	t=a;
+	a=b;
+	b=t;
+	printf("a=%d,b=%d\n", a,b);
 	
 	return 0;
 }
+
